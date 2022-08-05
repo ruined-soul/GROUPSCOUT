4bde4b98bfabf14cf56795686b07c7d4bcf1aa71
Ruined Soul <76905260+ruined-soul@users.noreply.github.com>
2022-08-05 11:03:14 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -28,8 +28,13 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 PM_START_TEXT = """
 **Hello {}, My Name is {}!** 
 I am an **SUPERB**  group management bot.
-You can find the list of available commands with /help.
 
+────────────────────────
+× sᴇʀᴠᴇʀ: `{}`
+× `{}` *user, ᴀᴄʀᴏss* `{}` *chats.*
+────────────────────────
+
+You can find the list of available commands with /help.
 """
 
 HELP_STRINGS = """
